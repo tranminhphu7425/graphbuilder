f4f0001012013d70b520e1127be6101f25277524
tranminhphu7425 <tranminhphu7.4.2005@gmail.com>
2025-03-04 14:27:51 +0700

    update detect mobile

--- src/tool/configfullpage.js
+++ src/tool/configfullpage.js
@@ -1,9 +1,6 @@
-function Configfullpage() {
-  // Phát hiện thiết bị di động
-  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    navigator.userAgent
-  ) || /Android|iPhone|iPad|iPod/i.test(navigator.platform);
+import { isMobile } from 'react-device-detect';
 
+function Configfullpage() {
   // Nếu là thiết bị di động, giảm kích thước văn bản đi 10%
   if (isMobile) {
     document.body.style.fontSize = "80%";
