13da377b95335401b9a8302141c96fa4af7c10dc
tranminhphu7425 <tranminhphu7.4.2005@gmail.com>
2025-03-04 14:21:53 +0700

    Update configfullpage.js

--- src/tool/configfullpage.js
+++ src/tool/configfullpage.js
@@ -1,15 +1,13 @@
 function Configfullpage() {
   // Phát hiện thiết bị di động
-  const isMobile =
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent
-    );
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  ) || /Android|iPhone|iPad|iPod/i.test(navigator.platform);
 
   // Nếu là thiết bị di động, giảm kích thước văn bản đi 10%
   if (isMobile) {
     document.body.style.fontSize = "80%";
   }
 }
-
 
 export default Configfullpage;
