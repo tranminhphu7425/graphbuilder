a62e2fadfdaa04137d9c2f53c162e967ec5a9650
tranminhphu7425 <tranminhphu7.4.2005@gmail.com>
2025-03-12 20:59:57 +0700

    Update configfullpage.js

--- src/tool/configfullpage.js
+++ src/tool/configfullpage.js
@@ -2,8 +2,12 @@ import { isMobile } from 'react-device-detect';
 
 function Configfullpage() {
   const isSmallScreen = window.innerWidth < 768;
-  // Nếu là thiết bị di động, giảm kích thước văn bản đi 10%
-  if (isMobile || isSmallScreen) {
+
+  // Kiểm tra xem có phải là WebView không
+  const isWebView = /(Android.*wv|iPhone.*wv|iPad.*wv)/i.test(navigator.userAgent);
+
+  // Nếu là thiết bị di động hoặc màn hình nhỏ hoặc là WebView, giảm kích thước văn bản đi 10%
+  if (isMobile || isSmallScreen || isWebView) {
     document.body.style.fontSize = "90%";
   }
 }
